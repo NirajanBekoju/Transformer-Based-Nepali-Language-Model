227fafdfc3c81d3ebc78c99bc467b98f6b4a5597
luitelNinja9 <nishantozil8@gmail.com>
2024-01-15 19:03:31 +0545

    Added word piece tokenizer

--- config.py
+++ config.py
@@ -1,16 +1,29 @@
-def getConfig():
-    model_config = {
-        "emsize" : 300, 
-        "d_hid" : 1024,
-        "nlayers" : 6,
-        "nhead" : 6, 
-        "dropout" : 0.2,
-        "bptt" : 64
-    }
+def getConfig(small = False):
+    if not small:
+        model_config = {
+            "emsize" : 300, 
+            "d_hid" : 1024,
+            "nlayers" : 6,
+            "nhead" : 6, 
+            "dropout" : 0.2,
+            "bptt" : 64
+        }
+        
+    else:
+        model_config = {
+            "emsize" : 300, 
+            "d_hid" : 800,
+            "nlayers" : 4,
+            "nhead" : 4, 
+            "dropout" : 0.05,
+            "bptt" : 16
+        }
 
     app_config = {
         "logs" : "tensorboard_logs",
-        "epochs" : 25,
+        "epochs" : 25 if not small else 10,
     }
+    
+ 
 
     return model_config, app_config
--- utils.py
+++ utils.py
@@ -1,8 +1,42 @@
 import regex as re
 import torch 
 import pickle
+from tokenizers import BertWordPieceTokenizer
 
-def preProcessText(text):
+
+
+def word_piece_decoder(text):
+    # Reverse of preProcessing specific to word_piece
+    text = re.sub('a',r'\u094D',text)
+    text = re.sub('y',r'\u0941',text)
+    text = re.sub('e',r'\u0942',text)
+    text = re.sub('i',r'\u0901',text)
+    text = re.sub('c',r'\u0902',text)
+    text = re.sub('r',r'\u0943',text)
+    text = re.sub('l',r'\u0947',text)
+    text = re.sub('o',r'\u094b',text)
+    text = re.sub('p',r'\u094c',text)
+    text = re.sub('k',r'\u0948',text)
+    
+    return text
+
+
+def word_piece_encoder(text):
+    #replace devnagari tokens that doesn't work well for the bert-wordpiece tokenizer    
+    text = re.sub(r'\u094D','a',text)
+    text = re.sub(r'\u0941','y',text)
+    text = re.sub(r'\u0942','e',text)
+    text = re.sub(r'\u0901','i',text)
+    text = re.sub(r'\u0902','c',text)
+    text = re.sub(r'\u0943','r',text)
+    text = re.sub(r'\u0947','l',text)
+    text = re.sub(r'\u094b','o',text)
+    text = re.sub(r'\u094c','p',text)
+    text = re.sub(r'\u0948','k',text)
+    
+    return text
+
+def preProcessText(text, tokenizer_type = 'default'):
     # put space in beteen the | -> devanagari danda to make it a separate word.
     text = re.sub(r'\s*[\u0964]\s*', r'\u0020\u0964\u0020', text)
     # put space around the question mark ?  to make it a separate word
@@ -15,14 +49,52 @@ def preProcessText(text):
     text = re.sub(r'[^\u0900-\u097F,?\s+]','', text) 
     # add space in between the devanagari numbers and replace number by <num> token
     text = re.sub(r'\s*[\u0966-\u0976]+\s*', '\u0020<num>\u0020', text)
+    
+    
+    if tokenizer_type == 'word_piece':
+        #replace devnagari tokens that doesn't work well for the bert-wordpiece tokenizer  
+        text = word_piece_encoder(text)
+
+        
     return text
 
-def getTokenizer():
-    tokenizer_dir = "tokenizer"
-    tokenizer_path = tokenizer_dir + "/tokenizer.pth"
-    vocab_path = tokenizer_dir + "/vocab.pkl"
-    loaded_tokenizer = torch.load(tokenizer_path)
-    with open(vocab_path, 'rb') as file:
-        loaded_vocab = pickle.load(file)
+
+def getTokenizer(tokenizer_type = 'default'):
+    tokenizer_dir = "tokenizers"
+    
+    
+    if tokenizer_type == 'default':
+        tokenizer_path = tokenizer_dir + "/tokenizer.pth"
+        vocab_path = tokenizer_dir + "/vocab.pkl"
+        loaded_tokenizer = torch.load(tokenizer_path)
+        with open(vocab_path, 'rb') as file:
+            loaded_vocab = pickle.load(file)
+            
+    elif tokenizer_type == 'word_piece':
+        tokenizer_path = tokenizer_dir + "/tokenizer_wp.pickle"
+    
+    if tokenizer_type != 'default':
+        with open(tokenizer_path, 'rb') as file:
+            loaded_tokenizer = pickle.load(file)
+            
+        loaded_vocab = loaded_tokenizer.get_vocab()
 
     return loaded_tokenizer, loaded_vocab
+
+def try_gpu(i=0):
+    if torch.cuda.device_count() >= i+1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+
+
+def split_list(l):
+    splitted_list = []
+    z = 0
+    for i,idx in enumerate(l):
+        if idx == 220:
+            splitted_list.append(l[z:i])
+            z = i+1
+    if z <= len(l)-1:
+        splitted_list.append(l[z:])
+    return splitted_list
